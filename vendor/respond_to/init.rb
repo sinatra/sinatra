4e1fffc0613cac72e967f9423b96e85a29be863d
blake.mizerany@gmail.com <blake.mizerany@gmail.com@95fbf360-2702-46e8-84d8-6de9ca90a270>
2007-09-11 23:16:38 +0000

    taking out for now

--- vendor/respond_to/init.rb
+++
@@ -1,5 +0,0 @@
-require File.dirname(__FILE__) + '/lib/responder'
-
-Sinatra::EventContext.send(:include, Sinatra::Responder)
-Sinatra::Event.send(:include, Sinatra::EventResponder)
-Sinatra::EventManager.send(:extend, Sinatra::DispatcherResponder)
