d599c45f5f4c4103cba74c534171ff6ad0bbfdfb
Blake Mizerany <blakemizerany@blake-mizeranys-computer.local>
2007-11-21 01:49:47 -0800

    routes working

---
+++ test/route_test.rb
@@ -0,0 +1,41 @@
+require File.dirname(__FILE__) + "/../lib/sinatra"
+require 'test/spec'
+require 'mocha'
+
+context "A Route in general" do
+    
+  specify "matches a path to a block" do
+    e = Sinatra::Route.new('/') do
+      'hello'
+    end
+    
+    result = e.match('/')
+    result.block.call.should.equal 'hello'
+    result.params.should.be.empty
+  end
+  
+  specify "matches with params from path" do
+    e = Sinatra::Route.new('/:name') do
+      'hello again'
+    end
+
+    # spaces should work
+    result = e.match('/blake%20mizerany')
+    result.should.not.be.nil
+    result.block.call.should.equal 'hello again'
+    result.params.should.equal :name => 'blake mizerany'
+  end
+  
+  specify "matches multiple vars in path" do
+    e = Sinatra::Route.new('/:name/:age') do
+      'hello again'
+    end
+
+    # spaces should work
+    result = e.match('/blake%20mizerany/25')
+    result.should.not.be.nil
+    result.block.call.should.equal 'hello again'
+    result.params.should.equal :name => 'blake mizerany', :age => '25'
+  end
+    
+end
