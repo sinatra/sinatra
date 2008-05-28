dc6e32a5cea29e9055463f6eff84f2b829a19c1c
Chris Schneider <chris@christopher-schneider.com>
2008-05-27 20:09:45 -0600

    Add :locals option for erb.  General approach and some of the code was borrowed from rails (compilable.rb)

--- lib/sinatra.rb
+++ lib/sinatra.rb
@@ -576,9 +576,19 @@ module Sinatra
     private 
     
       def render_erb(content, options = {})
-        ::ERB.new(content).result(binding)
+        locals_opt = options.delete(:locals) || {}
+
+        locals_code = ""
+        locals_hash = {} 
+        locals_opt.each do |key, value|
+          locals_code << "#{key} = locals_hash[:#{key}]\n"
+          locals_hash[:"#{key}"] = value
+        end
+ 
+        body = ::ERB.new(content).src
+        eval("#{locals_code}#{body}", binding)
       end
-      
+
   end
 
   module Haml
