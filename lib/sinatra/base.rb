54b9ecf3e1e9efce00f8381fe0bbca4b80acc537
Ryan Tomayko <rtomayko@gmail.com>
2008-12-22 23:45:21 -0800

    ensure that Content-Length is set when possible

--- lib/sinatra/base.rb
+++ lib/sinatra/base.rb
@@ -13,6 +13,31 @@ module Sinatra
   end
 
   class Response < Rack::Response
+    def initialize
+      @status, @body = 200, []
+      @header = Rack::Utils::HeaderHash.new({'Content-Type' => 'text/html'})
+    end
+
+    def write(str)
+      @body << str.to_s
+      str
+    end
+
+    def finish
+      @body = block if block_given?
+      if [204, 304].include?(status.to_i)
+        header.delete "Content-Type"
+        [status.to_i, header.to_hash, []]
+      else
+        body = @body || []
+        body = [body] if body.respond_to? :to_str
+        if header["Content-Length"].nil? && body.respond_to?(:to_ary)
+          header["Content-Length"] = body.to_ary.
+            inject(0) { |len, part| len + part.length }.to_s
+        end
+        [status.to_i, header.to_hash, body]
+      end
+    end
   end
 
   class NotFound < NameError # :)
