ddb1a2f61265da0728f18aa75a08cc20d2c82484
Andreas Haller <andreashaller@gmail.com>
2009-01-19 23:52:08 +0700

    using ":environment" instead of ":env" in test module
    fix for "warning: The :env option is deprecated; use :environment instead."

--- lib/sinatra/test/unit.rb
+++ lib/sinatra/test/unit.rb
@@ -4,7 +4,7 @@ require 'test/unit'
 Test::Unit::TestCase.send :include, Sinatra::Test
 
 Sinatra::Default.set(
-  :env => :test,
+  :environment => :test,
   :run => false,
   :raise_errors => true,
   :logging => false
