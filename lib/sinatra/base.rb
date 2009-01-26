64c8297f6084c867509a61e7ad91171fb6fa44ce
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2009-01-25 23:11:15 -0200

    Refactor Sinatra::Delegator so it allows adding methods dinamically in a clean way

--- lib/sinatra/base.rb
+++ lib/sinatra/base.rb
@@ -887,20 +887,21 @@ module Sinatra
   end
 
   module Delegator
-    METHODS = %w[
-      get put post delete head template layout before error not_found
-      configures configure set set_option set_options enable disable use
-      development? test? production? use_in_file_templates!
-    ]
-
-    METHODS.each do |method_name|
-      eval <<-RUBY, binding, '(__DELEGATE__)', 1
-        def #{method_name}(*args, &b)
-          ::Sinatra::Application.#{method_name}(*args, &b)
-        end
-        private :#{method_name}
-      RUBY
+    def self.delegate(*methods)
+      methods.each do |method_name|
+        eval <<-RUBY, binding, '(__DELEGATE__)', 1
+          def #{method_name}(*args, &b)
+            ::Sinatra::Application.#{method_name}(*args, &b)
+          end
+          private :#{method_name}
+        RUBY
+      end
     end
+
+    delegate :get, :put, :post, :delete, :head, :template, :layout, :before,
+             :error, :not_found, :configures, :configure, :set, :set_option,
+             :set_options, :enable, :disable, :use, :development?, :test?,
+             :production?, :use_in_file_templates!
   end
 
   def self.new(base=Base, options={}, &block)
