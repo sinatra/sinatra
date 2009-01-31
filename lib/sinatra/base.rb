c8f1c7cb4d66465d7d198388324a01d9c06463e7
Ryan Tomayko <rtomayko@gmail.com>
2009-01-30 17:22:09 -0800

    Fix passing a method as a block under 1.9

--- lib/sinatra/base.rb
+++ lib/sinatra/base.rb
@@ -514,7 +514,7 @@ module Sinatra
           metadef("#{option}?") { !!__send__(option) }
           metadef("#{option}=") { |val| set(option, Proc.new{val}) }
         elsif value == self && option.respond_to?(:to_hash)
-          option.to_hash.each(&method(:set))
+          option.to_hash.each { |k,v| set(k, v) }
         elsif respond_to?("#{option}=")
           __send__ "#{option}=", value
         else
