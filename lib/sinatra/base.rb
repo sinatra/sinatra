42cb875dc7b9ffc76326b3b5d7f01fbef6f62c63
Sam Roberts <vieuxtech@gmail.com>
2009-02-17 09:27:12 -0200

    Put less stuff on STDOUT when using a CGI handler

--- lib/sinatra/base.rb
+++ lib/sinatra/base.rb
@@ -723,12 +723,12 @@ module Sinatra
         handler      = detect_rack_handler
         handler_name = handler.name.gsub(/.*::/, '')
         puts "== Sinatra/#{Sinatra::VERSION} has taken the stage " +
-          "on #{port} for #{environment} with backup from #{handler_name}"
+          "on #{port} for #{environment} with backup from #{handler_name}" unless handler_name =~/cgi/i
         handler.run self, :Host => host, :Port => port do |server|
           trap(:INT) do
             ## Use thins' hard #stop! if available, otherwise just #stop
             server.respond_to?(:stop!) ? server.stop! : server.stop
-            puts "\n== Sinatra has ended his set (crowd applauds)"
+            puts "\n== Sinatra has ended his set (crowd applauds)" unless handler_name =~/cgi/i
           end
         end
       rescue Errno::EADDRINUSE => e
