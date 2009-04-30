043e987a33af962f7d8b59445c807a5d13fc473d
Ryan Tomayko <rtomayko@gmail.com>
2009-04-30 11:10:08 -0700

    Add RUBY_IGNORE_CALLERS to CALLERS_TO_IGNORE
    Rubinius sets this to an array of kernel call
    path patterns.

--- lib/sinatra/base.rb
+++ lib/sinatra/base.rb
@@ -940,6 +940,9 @@ module Sinatra
         /active_support/,      # active_support require hacks
       ] unless self.const_defined?('CALLERS_TO_IGNORE')
 
+      # add rubinius (and hopefully other VM impls) ignore patterns ...
+      CALLERS_TO_IGNORE.concat(RUBY_IGNORE_CALLERS) if defined?(RUBY_IGNORE_CALLERS)
+
       # Like Kernel#caller but excluding certain magic entries and without
       # line / method information; the resulting array contains filenames only.
       def caller_files
