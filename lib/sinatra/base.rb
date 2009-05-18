da29d46836fed83ec8d1a308d7937e685f2aa8fc
Simon Rozet <simon@rozet.name>
2009-05-18 12:09:39 +0200

    Get rid of unecessary monkey-patch

--- lib/sinatra/base.rb
+++ lib/sinatra/base.rb
@@ -1106,13 +1106,3 @@ module Sinatra
     Default.helpers(*extensions, &block)
   end
 end
-
-class String #:nodoc:
-  # Define String#each under 1.9 for Rack compatibility. This should be
-  # removed once Rack is fully 1.9 compatible.
-  alias_method :each, :each_line  unless ''.respond_to? :each
-
-  # Define String#bytesize as an alias to String#length for Ruby 1.8.6 and
-  # earlier.
-  alias_method :bytesize, :length unless ''.respond_to? :bytesize
-end
