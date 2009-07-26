e62532a86ad6af7ba3449ec42c4ad7a02c16d424
Ryan Tomayko <rtomayko@gmail.com>
2009-07-26 13:04:52 -0700

    Fix app_file detect when under /usr/lib/ruby/1.8 (debian, etc.) [#249]

--- lib/sinatra/base.rb
+++ lib/sinatra/base.rb
@@ -951,7 +951,7 @@ module Sinatra
 
     public
       CALLERS_TO_IGNORE = [
-        /lib\/sinatra.*\.rb$/, # all sinatra code
+        /\/sinatra(\/(base|main|showexceptions|compat))?\.rb$/, # all sinatra code
         /\(.*\)/,              # generated code
         /custom_require\.rb$/, # rubygems require hacks
         /active_support/,      # active_support require hacks
