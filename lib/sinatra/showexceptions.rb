d8af6c48d23ef2e4e2d393b29143f0d577a6f20b
Ryan Tomayko <rtomayko@gmail.com>
2009-10-26 04:32:58 -0700

    add height/width to showexceptions spill image

--- lib/sinatra/showexceptions.rb
+++ lib/sinatra/showexceptions.rb
@@ -137,7 +137,7 @@ TEMPLATE = <<HTML
 <body>
   <div id="wrap">
     <div id="header">
-      <img src="/__sinatra__/500.png" alt="application error" />
+      <img src="/__sinatra__/500.png" alt="application error" height="161" width="313" />
       <div id="summary">
         <h1><strong><%=h exception.class %></strong> at <strong><%=h path %>
           </strong></h1>
