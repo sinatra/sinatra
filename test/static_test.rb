9a01e3d4920c675219cf45d01532497c4af5dfbf
Jens Alfke <jens@mooseyard.com>
2010-10-17 12:31:07 -0700

    Re-implement byte-range support for static files.
    Correct handling of "Range:" request header. Replaces buggy implementation (480b1e8, 44ab090) that was recently backed out.
    Fixes #93.
    
    NOTE: Does not yet support multiple ranges (e.g. "bytes=1-10,20-30") because that requires sending a multipart response, which is more complex than I want to get into now.
    
    Signed-off-by: Konstantin Haase <konstantin.mailinglists@googlemail.com>

--- test/static_test.rb
+++ test/static_test.rb
@@ -90,4 +90,68 @@ class StaticTest < Test::Unit::TestCase
     get "/../#{File.basename(__FILE__)}"
     assert not_found?
   end
+
+  def test_valid_range(http_range, range, path, file)
+    request = Rack::MockRequest.new(@app)
+    response = request.get("/#{File.basename(path)}", 'HTTP_RANGE' => http_range)
+
+    should_be = file[range]
+    expected_range = "bytes #{range.begin}-#{range.end}/#{file.length}"
+
+    assert_equal 206,response.status, "Should be HTTP/1.1 206 Partial content"
+    assert_equal should_be.length, response.body.length, "Unexpected response length for #{http_range}"
+    assert_equal should_be, response.body, "Unexpected response data for #{http_range}"
+    assert_equal should_be.length.to_s, response['Content-Length'], "Incorrect Content-Length for #{http_range}"
+    assert_equal expected_range, response['Content-Range'], "Incorrect Content-Range for #{http_range}"
+  end
+
+  it 'handles valid byte ranges correctly' do
+    # Use the biggest file in this dir so we can test ranges > 8k bytes. (StaticFile sends in 8k chunks.)
+    path = File.dirname(__FILE__) + '/helpers_test.rb'  # currently 16k bytes
+    file = File.read(path)
+    length = file.length
+    assert length > 9000, "The test file #{path} is too short (#{length} bytes) to run these tests"
+
+    [0..0, 42..88, 1234..1234, 100..9000, 0..(length-1), (length-1)..(length-1)].each do |range|
+      test_valid_range("bytes=#{range.begin}-#{range.end}", range, path, file)
+    end
+
+    [0, 100, length-100, length-1].each do |start|
+      test_valid_range("bytes=#{start}-", (start..length-1), path, file)
+    end
+
+    [1, 100, length-100, length-1, length].each do |range_length|
+      test_valid_range("bytes=-#{range_length}", (length-range_length..length-1), path, file)
+    end
+
+    # Some valid ranges that exceed the length of the file:
+    test_valid_range("bytes=100-999999", (100..length-1), path, file)
+    test_valid_range("bytes=100-#{length}", (100..length-1), path, file)
+    test_valid_range("bytes=-#{length}", (0..length-1), path, file)
+    test_valid_range("bytes=-#{length+1}", (0..length-1), path, file)
+    test_valid_range("bytes=-999999", (0..length-1), path, file)
+  end
+
+  it 'correctly ignores syntactically invalid range requests' do
+    # ...and also ignores multi-range requests, which aren't supported yet
+    ["bytes=45-40", "bytes=IV-LXVI", "octets=10-20", "bytes=-", "bytes=1-2,3-4"].each do |http_range|
+      request = Rack::MockRequest.new(@app)
+      response = request.get("/#{File.basename(__FILE__)}", 'HTTP_RANGE' => http_range)
+
+      assert_equal 200,response.status, "Invalid range '#{http_range}' should be ignored"
+      assert_equal nil,response['Content-Range'], "Invalid range '#{http_range}' should be ignored"
+    end
+  end
+
+  it 'returns error 416 for unsatisfiable range requests' do
+    # An unsatisfiable request is one that specifies a start that's at or past the end of the file.
+    length = File.read(__FILE__).length
+    ["bytes=888888-", "bytes=888888-999999", "bytes=#{length}-#{length}"].each do |http_range|
+      request = Rack::MockRequest.new(@app)
+      response = request.get("/#{File.basename(__FILE__)}", 'HTTP_RANGE' => http_range)
+
+      assert_equal 416,response.status, "Unsatisfiable range '#{http_range}' should return 416"
+      assert_equal "bytes */#{length}",response['Content-Range'], "416 response should include actual length"
+    end
+  end
 end
