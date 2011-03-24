fd19d70134495d36ad43cd839b84b7b11e1e7c0d
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-03-24 08:46:25 +0100

    infrastructure

---
+++ sinatra-contrib/lib/sinatra/contrib.rb
@@ -0,0 +1,27 @@
+require 'sinatra/contrib/setup'
+
+module Sinatra
+  module Contrib
+    ##
+    # Common middleware that doesn't bring run time overhead if not used
+    # or breaks if external dependencies are missing. Will extend
+    # Sinatra::Application by default.
+    module Common
+      register :ConfigFile
+      register :Decompile
+      register :Namespace
+      helpers :LinkHeader
+    end
+
+    ##
+    # Other extensions you don't want to be loaded unless needed.
+    module Custom
+    end
+
+    ##
+    # Stuff that aren't Sinatra extensions, technically.
+    autoload :TestHelpers
+  end
+
+  register Sinatra::Contrib::Common
+end
