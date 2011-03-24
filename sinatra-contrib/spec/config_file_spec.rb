ec248a8b5aa1a85a08e8007f69facb5337c1c6d5
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-03-24 10:25:00 +0100

    add reimplemenation of sinatra-config-file

---
+++ sinatra-contrib/spec/config_file_spec.rb
@@ -0,0 +1,47 @@
+require 'backports'
+require_relative 'spec_helper'
+
+describe Sinatra::ConfigFile do
+  attr_accessor :settings
+  def config_file(*args, &block)
+    test = self
+    mock_app do
+      test.settings = settings
+      register Sinatra::ConfigFile
+      set :root, File.expand_path('../config_file', __FILE__)
+      instance_eval(&block) if block
+      config_file(*args)
+    end
+  end
+
+  it 'should set options from a simple config_file' do
+    config_file 'key_value.yml'
+    settings.foo.should == 'bar'
+    settings.something.should == 42
+  end
+
+  it 'should create indifferent hashes' do
+    config_file 'key_value.yml'
+    settings.nested['a'].should == 1
+    settings.nested[:a].should == 1
+  end
+
+  it 'should recognize env specific settings per file' do
+    config_file 'with_envs.yml'
+    settings.foo.should == 'test'
+  end
+
+  it 'should recognize env specific settings per setting' do
+    config_file 'with_nested_envs.yml'
+    settings.database[:adapter].should == 'sqlite'
+  end
+
+  it 'should not set present values to nil if the current env is missing' do
+    # first let's check the test is actually working properly
+    config_file('missing_env.yml') { set :foo => 42, :environment => :production }
+    settings.foo.should == 10
+    # now test it
+    config_file('missing_env.yml') { set :foo => 42, :environment => :test }
+    settings.foo.should == 42
+  end
+end
