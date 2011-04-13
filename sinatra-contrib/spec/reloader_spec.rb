a852f357d7ba322dd1c347e72e9f852ce4ce11a0
Gabriel Andretta <ohhgabriel@gmail.com>
2011-04-13 15:38:35 -0300

    add specs for .dont_reload and .also_reload

--- sinatra-contrib/spec/reloader_spec.rb
+++ sinatra-contrib/spec/reloader_spec.rb
@@ -32,14 +32,18 @@ describe Sinatra::Reloader do
   end
 
   def update_app_file(options={})
-    original_mtime = File.mtime(app_file_path)
-    begin
-      write_app_file(options)
-      sleep 0.1
-    end until original_mtime != File.mtime(app_file_path)
+    update_file(app_file_path) { write_app_file(options) }
   end
 
-  def build_and_require_example_app(options={})
+  def update_file(path)
+    original_mtime = File.mtime(path)
+    begin
+      yield(path)
+      sleep 0.1
+    end until original_mtime != File.mtime(path)
+  end
+
+  def setup_example_app(options={})
     @@example_app_counter ||= 0
     @@example_app_counter += 1
 
@@ -50,43 +54,119 @@ describe Sinatra::Reloader do
     self.app = app_const
   end
 
-
-  before(:each) do
-    build_and_require_example_app(
-      :routes => ['get("/foo") { erb :foo }'],
-      :inline_templates => { :foo => 'foo' }
-    )
-  end
-
   after(:all) { FileUtils.rm_rf(tmp_dir) }
 
-  it "doesn't mess up the application" do
-    get('/foo').body.should == 'foo'
+  describe "default reloading mechanism" do
+    before(:each) do
+      setup_example_app(
+        :routes => ['get("/foo") { erb :foo }'],
+        :inline_templates => { :foo => 'foo' }
+      )
+    end
+
+    it "doesn't mess up the application" do
+      get('/foo').body.should == 'foo'
+    end
+
+    it "knows when a route has been modified" do
+      update_app_file(:routes => ['get("/foo") { "bar" }'])
+      get('/foo').body.should == 'bar'
+    end
+
+    it "knows when a route has been added" do
+      update_app_file(
+        :routes => ['get("/foo") { "foo" }', 'get("/bar") { "bar" }']
+      )
+      get('/foo').body.should == 'foo'
+      get('/bar').body.should == 'bar'
+    end
+
+    it "knows when a route has been removed" do
+      update_app_file(:routes => ['get("/bar") { "bar" }'])
+      get('/foo').status.should == 404
+    end
+
+    it "reloads inline templates" do
+      update_app_file(
+        :routes => ['get("/foo") { erb :foo }'],
+        :inline_templates => { :foo => 'bar' }
+      )
+      get('/foo').body.should == 'bar'
+    end
   end
 
-  it "knows when a route has been modified" do
-    update_app_file(:routes => ['get("/foo") { "bar" }'])
-    get('/foo').body.should == 'bar'
+  describe ".dont_reload" do
+    before(:each) do
+      setup_example_app(
+        :routes => ['get("/foo") { erb :foo }'],
+        :inline_templates => { :foo => 'foo' }
+      )
+    end
+
+    it "allows to specify a file to stop from being reloaded" do
+      app_const.dont_reload app_file_path
+      update_app_file(:routes => ['get("/foo") { "bar" }'])
+      get('/foo').body.should == 'foo'
+    end
+
+    it "allows to specify a glob to stop matching files from being reloaded" do
+      app_const.dont_reload '**/*.rb'
+      update_app_file(:routes => ['get("/foo") { "bar" }'])
+      get('/foo').body.should == 'foo'
+    end
+
+    it "doesn't interfere with other application's reloading policy" do
+      pending
+      app_const.dont_reload '**/*.rb'
+      setup_example_app(:routes => ['get("/foo") { "foo" }'])
+      update_app_file(:routes => ['get("/foo") { "bar" }'])
+      get('/foo').body.should == 'bar'
+    end
   end
 
-  it "knows when a route has been added" do
-    update_app_file(
-      :routes => ['get("/foo") { "foo" }', 'get("/bar") { "bar" }']
-    )
-    get('/foo').body.should == 'foo'
-    get('/bar').body.should == 'bar'
-  end
+  describe ".also_reload" do
+    before(:each) do
+      setup_example_app(:routes => ['get("/foo") { Foo.foo }'])
+      @foo_path = File.join(tmp_dir, 'foo.rb')
+      File.open(@foo_path, 'w') do |f|
+        f.write 'class Foo; def self.foo() "foo" end end'
+      end
+      $LOADED_FEATURES.delete @foo_path
+      require @foo_path
+      app_const.also_reload @foo_path
+    end
 
-  it "knows when a route has been removed" do
-    update_app_file(:routes => ['get("/bar") { "bar" }'])
-    get('/foo').status.should == 404
-  end
+    it "allows to specify a file to be reloaded" do
+      get('/foo').body.should == 'foo'
+      update_file(@foo_path) do |path|
+        File.open(path, 'w') do |f|
+          f.write 'class Foo; def self.foo() "bar" end end'
+        end
+      end
+      get('/foo').body.should == 'bar'
+    end
 
-  it "reloads inline templates" do
-    update_app_file(
-      :routes => ['get("/foo") { erb :foo }'],
-      :inline_templates => { :foo => 'bar' }
-    )
-    get('/foo').body.should == 'bar'
+    it "allows to specify glob to reaload matching files" do
+      get('/foo').body.should == 'foo'
+      update_file(@foo_path) do |path|
+        File.open(path, 'w') do |f|
+          f.write 'class Foo; def self.foo() "bar" end end'
+        end
+      end
+      get('/foo').body.should == 'bar'
+    end
+
+    it "doesn't interfere with other application's reloading policy" do
+      pending
+      app_const.also_reload '**/*.rb'
+      setup_example_app(:routes => ['get("/foo") { Foo.foo }'])
+      get('/foo').body.should == 'foo'
+      update_file(@foo_path) do |path|
+        File.open(path, 'w') do |f|
+          f.write 'class Foo; def self.foo() "bar" end end'
+        end
+      end
+      get('/foo').body.should == 'foo'
+    end
   end
 end
