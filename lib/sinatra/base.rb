7079399ce1a3623de9dea780e161208798cf93cc
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-04-13 15:50:42 +0200

    1.3.0.c release

--- lib/sinatra/base.rb
+++ lib/sinatra/base.rb
@@ -6,7 +6,7 @@ require 'sinatra/showexceptions'
 require 'tilt'
 
 module Sinatra
-  VERSION = '1.3.0.b'
+  VERSION = '1.3.0.c'
 
   # The request object. See Rack::Request for more info:
   # http://rack.rubyforge.org/doc/classes/Rack/Request.html
