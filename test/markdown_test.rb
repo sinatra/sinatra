a9d89d28a515d68cd7c6db91dca49bbae7e21b05
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-04-15 11:14:23 +0200

    add markdown, rdoc and textile nesting tests

--- test/markdown_test.rb
+++ test/markdown_test.rb
@@ -9,11 +9,6 @@ MarkdownTest = proc do
     get '/'
   end
 
-  def assert_like(a,b)
-    pattern = /\s*\n\s*| id=['"][^"']*["']/
-    assert_equal a.strip.gsub(pattern, ""), b.strip.gsub(pattern, "")
-  end
-
   def setup
     Tilt.prefer engine, 'markdown', 'mkd', 'md'
     super
@@ -40,6 +35,36 @@ MarkdownTest = proc do
   it "raises error if template not found" do
     mock_app { get('/') { markdown :no_such_template } }
     assert_raise(Errno::ENOENT) { get('/') }
+  end
+
+  it "renders with inline layouts" do
+    mock_app do
+      layout { 'THIS. IS. #{yield.upcase}!' }
+      get('/') { markdown 'Sparta', :layout_engine => :str }
+    end
+    get '/'
+    assert ok?
+    assert_like 'THIS. IS. <P>SPARTA</P>!', body
+  end
+
+  it "renders with file layouts" do
+    markdown_app { markdown 'Hello World', :layout => :layout2, :layout_engine => :erb }
+    assert ok?
+    assert_body "ERB Layout!\n<p>Hello World</p>"
+  end
+
+  it "can be used in a nested fashion for partials and whatnot" do
+    mock_app do
+      template(:inner) { "hi" }
+      template(:outer) { "<outer><%= markdown :inner %></outer>" }
+      get '/' do
+        erb :outer
+      end
+    end
+
+    get '/'
+    assert ok?
+    assert_like '<outer><p>hi</p></outer>', body
   end
 end
 
--- test/rdoc_test.rb
+++ test/rdoc_test.rb
@@ -28,6 +28,36 @@ class RdocTest < Test::Unit::TestCase
     mock_app { get('/') { rdoc :no_such_template } }
     assert_raise(Errno::ENOENT) { get('/') }
   end
+
+  it "renders with inline layouts" do
+    mock_app do
+      layout { 'THIS. IS. #{yield.upcase}!' }
+      get('/') { rdoc 'Sparta', :layout_engine => :str }
+    end
+    get '/'
+    assert ok?
+    assert_like 'THIS. IS.<P>SPARTA</P>!', body
+  end
+
+  it "renders with file layouts" do
+    rdoc_app { rdoc 'Hello World', :layout => :layout2, :layout_engine => :erb }
+    assert ok?
+    assert_body "ERB Layout!\n<p>Hello World</p>"
+  end
+
+  it "can be used in a nested fashion for partials and whatnot" do
+    mock_app do
+      template(:inner) { "hi" }
+      template(:outer) { "<outer><%= rdoc :inner %></outer>" }
+      get '/' do
+        erb :outer
+      end
+    end
+
+    get '/'
+    assert ok?
+    assert_like '<outer><p>hi</p></outer>', body
+  end
 end
 
 rescue LoadError
