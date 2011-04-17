35cc04500a74b0f4229ddc3d1eed0d272d7058ca
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-04-17 13:55:13 +0200

    bump version

--- lib/sinatra/base.rb
+++ lib/sinatra/base.rb
@@ -7,7 +7,7 @@ require 'sinatra/showexceptions'
 require 'tilt'
 
 module Sinatra
-  VERSION = '1.2.3'
+  VERSION = '1.2.4'
 
   # The request object. See Rack::Request for more info:
   # http://rack.rubyforge.org/doc/classes/Rack/Request.html
