81c8e77ae71ffc6ddb95f4e7a32de465b8fcf1b9
Gabriel Andretta <ohhgabriel@gmail.com>
2011-04-19 14:46:55 -0300

    add docs to Sinatra::ConfigFile

--- sinatra-contrib/lib/sinatra/config_file.rb
+++ sinatra-contrib/lib/sinatra/config_file.rb
@@ -2,16 +2,49 @@ require 'sinatra/base'
 require 'yaml'
 
 module Sinatra
-  ##
-  # = Sinatra::ConfigFile
+  # Extension to load the application's configuration from YAML files.
+  # It automatically detects if files contain environment specific
+  # settings and it will use the corresponding to the current one.
   #
-  # Extension to load configruation from YAML files.
-  # Automatically detects if files contain env specific configuration.
+  # There are two ways to provide environment specific configuration:
+  # at the file level and at the setting level.  They are illustrated,
+  # repsectively, as follows:
+  #
+  #   development:
+  #     foo: development
+  #   test:
+  #     foo: test
+  #   production:
+  #     foo: production
+  #
+  # and
+  #
+  #   foo:
+  #     development: development
+  #     test: test
+  #     production: production
+  #
+  # In either case, the setting +foo+ will return the environment
+  # name.  Be aware that if you have a different environment, besides
+  # development, test and production, you will also need to adjust the
+  # +environments+ setting.  For instance, when you also have a
+  # staging environment:
+  #
+  #   set :environments, %w{development test production staging}
+  #
+  # If you try to get the value for a setting that hasn't been defined
+  # in the config file for the current environment, you will get
+  # whatever it was set to in the application.
   module ConfigFile
+    # When the extension is registered sets the +environments+ setting
+    # to the typical environments: development, test and production.
     def self.registered(base)
       base.set :environments, %w[test production development]
     end
 
+    # Loads the configuration from the YAML files whose +paths+ are
+    # passed as arguments, filtering the settings for the current
+    # environment.
     def config_file(*paths)
       Dir.chdir(root || '.') do
         paths.each do |pattern|
@@ -29,6 +62,13 @@ module Sinatra
 
     private
 
+    # Given a +hash+ with some application configuration it returns
+    # the settings applicable to the current environment.  Note that
+    # this can only be done when all the keys of +hash+ are
+    # environment names included in the +environments+ setting (which
+    # is an Array of Strings).  Also, the returned config is a
+    # indifferently accessible Hash, which means that you can get its
+    # values using Strings or Symbols as keys.
     def config_for_env(hash)
       if hash.respond_to? :keys and hash.keys.all? { |k| environments.include? k.to_s }
         hash = hash[environment.to_s] || hash[environment.to_sym]
