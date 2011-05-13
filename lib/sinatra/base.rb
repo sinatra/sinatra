656d33688cf0219e2ed1a600a9ee6a76deaf1e12
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-05-13 09:48:37 +0200

    Response#finish was introduced in Rack 1.2

--- lib/sinatra/base.rb
+++ lib/sinatra/base.rb
@@ -59,21 +59,6 @@ module Sinatra
   # http://rack.rubyforge.org/doc/classes/Rack/Response.html
   # http://rack.rubyforge.org/doc/classes/Rack/Response/Helpers.html
   class Response < Rack::Response
-    def finish
-      @body = block if block_given?
-      if [204, 304].include?(status.to_i)
-        header.delete "Content-Type"
-        [status.to_i, header.to_hash, []]
-      else
-        body = @body || []
-        body = [body] if body.respond_to? :to_str
-        if body.respond_to?(:to_ary)
-          header["Content-Length"] = body.to_ary.
-            inject(0) { |len, part| len + Rack::Utils.bytesize(part) }.to_s
-        end
-        [status.to_i, header.to_hash, body]
-      end
-    end
   end
 
   class NotFound < NameError #:nodoc:
