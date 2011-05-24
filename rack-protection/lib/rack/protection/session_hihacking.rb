37c4d91c69a572a91f7d845ca2389b138e3c0cd1
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-05-24 14:32:34 +0200

    note about firesheep

--- rack-protection/lib/rack/protection/session_hihacking.rb
+++ rack-protection/lib/rack/protection/session_hihacking.rb
@@ -8,7 +8,9 @@ module Rack
     # More infos::         http://en.wikipedia.org/wiki/Session_hijacking
     #
     # Tracks request properties like the user agent in the session and empties
-    # the session if those properties change.
+    # the session if those properties change. This essentially prevents attacks
+    # from Firesheep. Since all headers taken into consideration might be
+    # spoofed, too, this will not prevent all hijacking attempts.
     #
     # Not Yet Implemented!
     class SessionHijacking < Base
