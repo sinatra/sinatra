defd82d025e9875c17dc03d210d73e09fa65ca74
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-05-24 11:09:15 +0200

    docs for XSSHeader

--- rack-protection/lib/rack/protection/xss_header.rb
+++ rack-protection/lib/rack/protection/xss_header.rb
@@ -2,6 +2,14 @@ require 'rack/protection'
 
 module Rack
   module Protection
+    ##
+    # Sets X-XSS-Protection header to tell the browser to block attacks.
+    #
+    # Prevented attack::   Non-permanent XSS
+    # Supported browsers:: Internet Explorer >= 8
+    #
+    # Options:
+    # xss_mode:: How the browser should prevent the attack (default: `:block`)
     class XSSHeader < Base
       default_options :xss_mode => :block
 
