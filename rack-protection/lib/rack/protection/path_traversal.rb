5535bf89f589c607d84d786233713c5a85cd6c5b
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-05-24 17:59:33 +0200

    implement PathTraversal

--- rack-protection/lib/rack/protection/path_traversal.rb
+++ rack-protection/lib/rack/protection/path_traversal.rb
@@ -12,6 +12,20 @@ module Rack
     #
     # Not Yet Implemented!
     class PathTraversal < Base
+      def call(env)
+        path_was         = env["PATH_INFO"]
+        env["PATH_INFO"] = cleanup path_was
+        app.call env
+      ensure
+        env["PATH_INFO"] = path_was
+      end
+
+      def cleanup(path)
+        return cleanup("/" << path)[1..-1] unless path[0] == ?/
+        escaped = ::File.expand_path path.gsub('%2e', '.').gsub('%2f', '/')
+        escaped << '/' if escaped[-1] != ?/ and path =~ /\/\.{0,2}$/
+        escaped.gsub /\/\/+/, '/'
+      end
     end
   end
 end
--- rack-protection/spec/path_traversal_spec.rb
+++ rack-protection/spec/path_traversal_spec.rb
@@ -2,4 +2,22 @@ require File.expand_path('../spec_helper.rb', __FILE__)
 
 describe Rack::Protection::PathTraversal do
   it_behaves_like "any rack application"
+
+  context 'escaping' do
+    before do
+      mock_app { |e| [200, {'Content-Type' => 'text/plain'}, [e['PATH_INFO']]] }
+    end
+
+    %w[/foo/bar /foo/bar/ / /.f /a.x].each do |path|
+      it("does not touch #{path.inspect}") { get(path).body.should == path }
+    end
+
+    {
+      '/..' => '/', '/a/../b' => '/b', '/a/../b/' => '/b/', '/a/.' => '/a/',
+      '/%2e.' => '/', '/a/%2e%2e/b' => '/b', '/a%2f%2e%2e%2fb/' => '/b/',
+      '//' => '/', '/%2fetc%2fpasswd' => '/etc/passwd'
+    }.each do |a, b|
+      it("replaces #{a.inspect} with #{b.inspect}") { get(a).body.should == b }
+    end
+  end
 end
