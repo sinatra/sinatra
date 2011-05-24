b046e5fe379219771c14f7984dd12cd1f73661cc
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-05-24 11:12:59 +0200

    specs for FrameOptions

--- rack-protection/spec/frame_options_spec.rb
+++ rack-protection/spec/frame_options_spec.rb
@@ -2,4 +2,23 @@ require File.expand_path('../spec_helper.rb', __FILE__)
 
 describe Rack::Protection::FrameOptions do
   it_behaves_like "any rack application"
+
+  it 'should set the X-XSS-Protection' do
+    get('/').headers["X-Frame-Options"].should == "sameorigin"
+  end
+
+  it 'should allow changing the protection mode' do
+    # I have no clue what other modes are available
+    mock_app do
+      use Rack::Protection::FrameOptions, :frame_options => :deny
+      run DummyApp
+    end
+
+    get('/').headers["X-Frame-Options"].should == "deny"
+  end
+
+  it 'should not override the header if already set' do
+    mock_app with_headers("X-Frame-Options" => "allow")
+    get('/').headers["X-Frame-Options"].should == "allow"
+  end
 end
