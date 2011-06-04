b10b80c28ec0010bda744cbaed1cb1a3b25431b3
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-06-04 18:24:42 +0200

    have invoke raise ArgumentErrors instead of TypeErrors

--- lib/sinatra/base.rb
+++ lib/sinatra/base.rb
@@ -727,7 +727,7 @@ module Sinatra
       res = catch(:halt) { yield }
       res = [res] if Fixnum === res or String === res
       if Array === res and Fixnum === res.first
-        raise TypeError, "#{res.inspect} not supported" if res.length > 3
+        raise ArgumentError, "#{res.inspect} not supported" if res.length > 3
         status(res.shift)
         body(res.pop)
         headers(*res)
