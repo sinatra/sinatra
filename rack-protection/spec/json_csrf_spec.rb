7798cda96760c076e9c0238776f32bc6755398dc
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-06-20 15:47:58 +0200

    pending specs for json csrf

--- rack-protection/spec/json_csrf_spec.rb
+++ rack-protection/spec/json_csrf_spec.rb
@@ -2,7 +2,22 @@ require File.expand_path('../spec_helper.rb', __FILE__)
 
 describe Rack::Protection::JsonCsrf do
   it_behaves_like "any rack application"
-  it "denies get requests with json responses with a remote referrer"
-  it "accepts get requests with json responses with a local referrer"
-  it "accepts get requests with json responses with no referrer"
+
+  describe 'json response' do
+    before do
+      mock_app { |e| [200, {'Content-Type' => 'application/json'}, []]}
+    end
+
+    it "denies get requests with json responses with a remote referrer" do
+      get('/', {}, 'HTTP_REFERER' => 'http://evil.com').should_not be_ok
+    end
+
+    it "accepts get requests with json responses with a local referrer" do
+      get('/', {}, 'HTTP_REFERER' => '/').should be_ok
+    end
+
+    it "accepts get requests with json responses with no referrer" do
+      get('/', {}).should be_ok
+    end
+  end
 end
