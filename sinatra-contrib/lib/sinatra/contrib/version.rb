92619052b71469745e17bee1cca9d4a6d1a5010b
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-10-01 15:01:13 -0500

    have VERSION be a string

--- sinatra-contrib/lib/sinatra/contrib/version.rb
+++ sinatra-contrib/lib/sinatra/contrib/version.rb
@@ -4,42 +4,14 @@ module Sinatra
       VERSION
     end
 
-    module VERSION
-      extend Comparable
+    SIGNATURE = [1, 3, 1]
+    VERSION   = SIGNATURE.join('.')
 
-      MAJOR     = 1
-      MINOR     = 3
-      TINY      = 0
-      SIGNATURE = [MAJOR, MINOR, TINY]
-      STRING    = SIGNATURE.join '.'
-
-      def self.major; MAJOR  end
-      def self.minor; MINOR  end
-      def self.tiny;  TINY   end
-      def self.to_s;  STRING end
-
-      def self.hash
-        STRING.hash
-      end
-
-      def self.<=>(other)
-        other = other.split('.').map { |i| i.to_i } if other.respond_to? :split
-        SIGNATURE <=> Array(other)
-      end
-
-      def self.inspect
-        STRING.inspect
-      end
-
-      def self.respond_to?(meth, *)
-        return true if super
-        meth.to_s !~ /^__|^to_str$/ and STRING.respond_to? meth
-      end
-
-      def self.method_missing(meth, *args, &block)
-        return super unless STRING.respond_to?(meth)
-        STRING.send(meth, *args, &block)
-      end
+    VERSION.extend Comparable
+    def VERSION.<=>(other)
+      other = other.split('.').map { |i| i.to_i } if other.respond_to? :split
+      SIGNATURE <=> Array(other)
     end
   end
 end
+
