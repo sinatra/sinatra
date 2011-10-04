121e2bb1cfefefb464c5d83c1500fac33f35a94c
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-10-04 15:06:43 -0700

    add test that makes sure passingin on :track option works. related to #6.

--- rack-protection/spec/protection_spec.rb
+++ rack-protection/spec/protection_spec.rb
@@ -2,4 +2,19 @@ require File.expand_path('../spec_helper.rb', __FILE__)
 
 describe Rack::Protection do
   it_behaves_like "any rack application"
+
+  it 'passes on options' do
+    mock_app do
+      use Rack::Protection, :track => ['HTTP_FOO']
+      run proc { |e| [200, {'Content-Type' => 'text/plain'}, ['hi']] }
+    end
+
+    session = {:foo => :bar}
+    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_ENCODING' => 'a'
+    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_ENCODING' => 'b'
+    session[:foo].should be == :bar
+
+    get '/', {}, 'rack.session' => session, 'HTTP_FOO' => 'BAR'
+    session.should be_empty
+  end
 end
