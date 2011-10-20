78d5b902d17f7622acb2b1379011d0039c1d59a7
James Tucker <jftucker@gmail.com>
2011-10-19 23:27:24 -0700

    UBERKLASSE MUST BE ÜBER

--- lib/sinatra/base.rb
+++ lib/sinatra/base.rb
@@ -1459,6 +1459,8 @@ module Sinatra
     end
   end
 
+  UBERKLASSE = Base
+
   # Execution context for classic style (top-level) applications. All
   # DSL methods executed on main are delegated to this class.
   #
