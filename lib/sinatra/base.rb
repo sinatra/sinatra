318b1a54622b6af6070c019bd452129f96b5eaa6
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2012-03-07 16:29:59 +0100

    deprecate RUBY_IGNORE_CALLERS, fixes #444

--- lib/sinatra/base.rb
+++ lib/sinatra/base.rb
@@ -1466,8 +1466,11 @@ module Sinatra
         /src\/kernel\/bootstrap\/[A-Z]/                  # maglev kernel files
       ]
 
-      # add rubinius (and hopefully other VM impls) ignore patterns ...
-      CALLERS_TO_IGNORE.concat(RUBY_IGNORE_CALLERS) if defined?(RUBY_IGNORE_CALLERS)
+      # contrary to what the comment said previously, rubinius never supported this
+      if defined?(RUBY_IGNORE_CALLERS)
+        warn "RUBY_IGNORE_CALLERS is deprecated and will no longer be supported by Sinatra 2.0"
+        CALLERS_TO_IGNORE.concat(RUBY_IGNORE_CALLERS)
+      end
 
       # Like Kernel#caller but excluding certain magic entries and without
       # line / method information; the resulting array contains filenames only.
