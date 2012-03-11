d872ae3c471fa5f7e23b9b0b08229da20fa8a180
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2012-03-11 13:56:55 +0100

    revert 1.4.x specific patch

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -11,7 +11,6 @@ class IntegrationTest < Test::Unit::TestCase
   attr_accessor :server
 
   it('sets the app_file') { assert_equal server.app_file, server.get("/app_file") }
-  it('only extends main') { assert_equal "true", server.get("/mainonly") }
 
   it 'logs once in development mode' do
     random = "%064x" % Kernel.rand(2**256-1)
