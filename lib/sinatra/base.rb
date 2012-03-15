3066698afc7f923e9af12a99f46b3d001b957d24
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2012-03-15 09:56:18 +0100

    prevent streaming issue with Rack::Lint and friends, fixes #473

--- lib/sinatra/base.rb
+++ lib/sinatra/base.rb
@@ -87,6 +87,32 @@ module Sinatra
     end
   end
 
+  # Some Rack handlers (Thin, Rainbows!) implement an extended body object protocol, however,
+  # some middleware (namely Rack::Lint) will break it by not mirroring the methods in question.
+  # This middleware will detect an extended body object and will make sure it reaches the
+  # handler directly. We do this here, so our middleware and middleware set up by the app will
+  # still be able to run.
+  class ExtendedRack < Struct.new(:app)
+    def call(env)
+      result, callback = app.call(env), env['async.callback']
+      return result unless callback and async?(*result)
+      after_response { callback.call result }
+      throw :async
+    end
+
+    private
+
+    def after_response(&block)
+      raise NotImplementedError, "only supports EventMachine at the moment" unless defined? EventMachine
+      EventMachine.next_tick(&block)
+    end
+
+    def async?(status, headers, body)
+      return true if status == -1
+      body.respond_to? :callback and body.respond_to? :errback
+    end
+  end
+
   # Behaves exactly like Rack::CommonLogger with the notable exception that it does nothing,
   # if another CommonLogger is already in the middleware chane.
   class CommonLogger < Rack::CommonLogger
@@ -1361,6 +1387,7 @@ module Sinatra
 
     private
       def setup_default_middleware(builder)
+        builder.use ExtendedRack
         builder.use ShowExceptions       if show_exceptions?
         builder.use Rack::MethodOverride if method_override?
         builder.use Rack::Head
