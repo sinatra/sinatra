77934886c5f2dee86905e4366d2068c30316b6fb
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2012-03-18 13:25:06 +0100

    don't set -w with thin in integration tests

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -32,7 +32,7 @@ class IntegrationTest < Test::Unit::TestCase
   end
 
   it 'streams async' do
-    next unless server.name == 'thin'
+    next unless server.thin?
 
     Timeout.timeout(3) do
       chunks = []
@@ -50,7 +50,7 @@ class IntegrationTest < Test::Unit::TestCase
   end
 
   it 'streams async from subclass' do
-    next unless server.name == 'thin'
+    next unless server.thin?
 
     Timeout.timeout(3) do
       chunks = []
