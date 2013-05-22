df0effbd3c8e5ab56e7ddfbcf37aa723b4d7806e
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-05-22 14:01:31 +0300

    Isolate response class

--- lib/sinatra/base.rb
+++ lib/sinatra/base.rb
@@ -10,69 +10,11 @@ require 'uri'
 
 # other files we need
 require 'sinatra/request'
+require 'sinatra/response'
 require 'sinatra/showexceptions'
 require 'sinatra/version'
 
 module Sinatra
-  # The request object. See Rack::Request for more info:
-  # http://rack.rubyforge.org/doc/classes/Rack/Request.html
-
-  # The response object. See Rack::Response and Rack::Response::Helpers for
-  # more info:
-  # http://rack.rubyforge.org/doc/classes/Rack/Response.html
-  # http://rack.rubyforge.org/doc/classes/Rack/Response/Helpers.html
-  class Response < Rack::Response
-    def initialize(*)
-      super
-      headers['Content-Type'] ||= 'text/html'
-    end
-
-    def body=(value)
-      value = value.body while Rack::Response === value
-      @body = String === value ? [value.to_str] : value
-    end
-
-    def each
-      block_given? ? super : enum_for(:each)
-    end
-
-    def finish
-      result = body
-
-      if drop_content_info?
-        headers.delete "Content-Length"
-        headers.delete "Content-Type"
-      end
-
-      if drop_body?
-        close
-        result = []
-      end
-
-      if calculate_content_length?
-        # if some other code has already set Content-Length, don't muck with it
-        # currently, this would be the static file-handler
-        headers["Content-Length"] = body.inject(0) { |l, p| l + Rack::Utils.bytesize(p) }.to_s
-      end
-
-      [status.to_i, headers, result]
-    end
-
-    private
-
-    def calculate_content_length?
-      headers["Content-Type"] and not headers["Content-Length"] and Array === body
-    end
-
-    def drop_content_info?
-      status.to_i / 100 == 1 or drop_body?
-    end
-
-    def drop_body?
-      [204, 205, 304].include?(status.to_i)
-    end
-  end
-
   # Some Rack handlers (Thin, Rainbows!) implement an extended body object protocol, however,
   # some middleware (namely Rack::Lint) will break it by not mirroring the methods in question.
   # This middleware will detect an extended body object and will make sure it reaches the
