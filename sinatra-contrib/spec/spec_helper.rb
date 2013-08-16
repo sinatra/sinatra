79108a7d2f90e404d1f0d1b2eb7935f76bdf6c7a
Zachary Scott <zachary@zacharyscott.net>
2013-08-16 11:11:15 -0400

    get rid of stdlib warning

--- sinatra-contrib/spec/spec_helper.rb
+++ sinatra-contrib/spec/spec_helper.rb
@@ -2,6 +2,6 @@ ENV['RACK_ENV'] = 'test'
 require 'sinatra/contrib'
 
 RSpec.configure do |config|
-  config.expect_with :rspec, :stdlib
+  config.expect_with :rspec#, :stdlib
   config.include Sinatra::TestHelpers
 end
