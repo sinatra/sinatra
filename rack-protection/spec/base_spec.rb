7b23fad1dafdabfa7191178a740945f34bfaa87a
Jason Staten <jstaten07@gmail.com>
2014-01-23 15:29:16 -0700

    fix typoed header name

--- rack-protection/spec/base_spec.rb
+++ rack-protection/spec/base_spec.rb
@@ -11,28 +11,28 @@ describe Rack::Protection::Base do
   end
 
   describe "#referrer" do
-    it "Reads referrer from Referrer header" do
+    it "Reads referrer from Referer header" do
       env = {"HTTP_HOST" => "foo.com", "HTTP_REFERER" => "http://bar.com/valid"}
       subject.referrer(env).should == "bar.com"
     end
 
-    it "Reads referrer from Host header when Referrer header is relative" do
+    it "Reads referrer from Host header when Referer header is relative" do
       env = {"HTTP_HOST" => "foo.com", "HTTP_REFERER" => "/valid"}
       subject.referrer(env).should == "foo.com"
     end
 
-    it "Reads referrer from Host header when Referrer header is missing" do
+    it "Reads referrer from Host header when Referer header is missing" do
       env = {"HTTP_HOST" => "foo.com"}
       subject.referrer(env).should == "foo.com"
     end
 
-    it "Returns nil when Referrer header is missing and allow_empty_referrer is false" do
+    it "Returns nil when Referer header is missing and allow_empty_referrer is false" do
       env = {"HTTP_HOST" => "foo.com"}
       subject.options[:allow_empty_referrer] = false
       subject.referrer(env).should be_nil
     end
 
-    it "Returns nil when Referrer header is invalid" do
+    it "Returns nil when Referer header is invalid" do
       env = {"HTTP_HOST" => "foo.com", "HTTP_REFERER" => "http://bar.com/bad|uri"}
       subject.referrer(env).should be_nil
     end
