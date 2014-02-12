4f609e530a1c7ece88ed51e7bc7a944f46a13470
Nick Sutterer <apotonick@gmail.com>
2014-02-12 11:11:00 +1100

    fix whitespaces again.

--- sinatra-contrib/lib/sinatra/runner.rb
+++ sinatra-contrib/lib/sinatra/runner.rb
@@ -43,73 +43,73 @@ require 'timeout'
 #
 # For an example, check https://github.com/apotonick/roar/blob/master/test/test_helper.rb
 module Sinatra
-	class Runner
-	  def app_file
-	    File.expand_path("../server.rb", __FILE__)
-	  end
+  class Runner
+    def app_file
+      File.expand_path("../server.rb", __FILE__)
+    end
 
-	  def run
-	    #puts command
-	    @pipe     = start
-	    @started  = Time.now
-	    warn "#{server} up and running on port #{port}" if ping
-	  end
+    def run
+      #puts command
+      @pipe     = start
+      @started  = Time.now
+      warn "#{server} up and running on port #{port}" if ping
+    end
 
-	  def kill
-	    return unless pipe
-	    Process.kill("KILL", pipe.pid)
-	  rescue NotImplementedError
-	    system "kill -9 #{pipe.pid}"
-	  rescue Errno::ESRCH
-	  end
+    def kill
+      return unless pipe
+      Process.kill("KILL", pipe.pid)
+    rescue NotImplementedError
+      system "kill -9 #{pipe.pid}"
+    rescue Errno::ESRCH
+    end
 
-	  def get(url)
-	    Timeout.timeout(1) { open("http://127.0.0.1:#{port}#{url}").read }
-	  end
+    def get(url)
+      Timeout.timeout(1) { open("http://127.0.0.1:#{port}#{url}").read }
+    end
 
-	  def log
-	    @log ||= ""
-	    loop { @log <<  pipe.read_nonblock(1) }
-	  rescue Exception
-	    @log
-	  end
+    def log
+      @log ||= ""
+      loop { @log <<  pipe.read_nonblock(1) }
+    rescue Exception
+      @log
+    end
 
-	private
-	  attr_accessor :pipe
+  private
+    attr_accessor :pipe
 
-	  def start
-	    IO.popen(command)
-	  end
+    def start
+      IO.popen(command)
+    end
 
-	  def command # to be overwritten
-	    "bundle exec ruby #{app_file} -p #{port} -e production"
-	  end
+    def command # to be overwritten
+      "bundle exec ruby #{app_file} -p #{port} -e production"
+    end
 
-	  def ping(timeout=30)
-	    loop do
-	      return if alive?
-	      if Time.now - @started > timeout
-	        $stderr.puts command, log
-	        fail "timeout"
-	      else
-	        sleep 0.1
-	      end
-	    end
-	  end
+    def ping(timeout=30)
+      loop do
+        return if alive?
+        if Time.now - @started > timeout
+          $stderr.puts command, log
+          fail "timeout"
+        else
+          sleep 0.1
+        end
+      end
+    end
 
-	  def alive?
-	    3.times { get(ping_path) }
-	    true
-	  rescue Errno::ECONNREFUSED, Errno::ECONNRESET, EOFError, SystemCallError, OpenURI::HTTPError, Timeout::Error
-	    false
-	  end
+    def alive?
+      3.times { get(ping_path) }
+      true
+    rescue Errno::ECONNREFUSED, Errno::ECONNRESET, EOFError, SystemCallError, OpenURI::HTTPError, Timeout::Error
+      false
+    end
 
-	  def ping_path # to be overwritten
-	    '/ping'
-	  end
+    def ping_path # to be overwritten
+      '/ping'
+    end
 
-	  def port # to be overwritten
-	    4567
-	  end
-	end
+    def port # to be overwritten
+      4567
+    end
+  end
 end
