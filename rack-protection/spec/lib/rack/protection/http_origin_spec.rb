f39fad4d66a4b3db59a3e7e9548b02816efb4b3e
Maurizio De Santis <desantis.maurizio@gmail.com>
2014-09-03 02:19:12 +0200

    Move spec files to conventional locations

--- rack-protection/spec/lib/rack/protection/http_origin_spec.rb
+++ rack-protection/spec/lib/rack/protection/http_origin_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../spec_helper.rb', __FILE__)
+require 'spec_helper'
 
 describe Rack::Protection::HttpOrigin do
   it_behaves_like "any rack application"
--- rack-protection/spec/lib/rack/protection/protection_spec.rb
+++ rack-protection/spec/lib/rack/protection/protection_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../spec_helper.rb', __FILE__)
+require 'spec_helper'
 
 describe Rack::Protection do
   it_behaves_like "any rack application"
