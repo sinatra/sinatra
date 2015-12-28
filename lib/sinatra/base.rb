10f5eaf68386cf7c626c9a37f0f14528da0fca71
aidewoode <aidewoode@gmail.com>
2015-12-28 11:48:41 +0800

    simplify compile method's code

--- lib/sinatra/base.rb
+++ lib/sinatra/base.rb
@@ -1631,7 +1631,7 @@ module Sinatra
               ignore << escaped(c).join if c.match(/[\.@]/)
               patt = encoded(c)
               patt.gsub(/%[\da-fA-F]{2}/) do |match|
-                match.split(//).map! {|char| char =~ /[A-Z]/ ? "[#{char}#{char.tr('A-Z', 'a-z')}]" : char}.join
+                match.split(//).map! {|char| char =~ /[A-F]/ ? "[#{char}#{char.downcase}]" : char}.join
               end
             end
 
@@ -1691,7 +1691,7 @@ module Sinatra
         unsafe_patterns = unsafe_ignore.map! do |unsafe|
           chars = unsafe.split(//).map! do |char|
             if char =~ /[A-Z]/
-              char <<= char.tr('A-Z', 'a-z')
+              char <<= char.downcase
             end
             char
           end
