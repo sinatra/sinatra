e9c4580a2d7e7a27ca81484548e2d0d828d771c5
Zachary Scott <mail@zzak.io>
2016-07-23 13:16:16 +0900

    Use full path to sinatra-contrib build dir

--- .travis.sh
+++ .travis.sh
@@ -5,6 +5,6 @@ echo "Running sinatra tests..."
 bundle exec rake
 
 echo "Running sinatra-contrib tests..."
-cd sinatra-contrib
+cd $TRAVIS_BUILD_DIR/sinatra-contrib
 bundle install --jobs=3 --retry=3
 bundle exec rake
