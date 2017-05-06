9064fd338c80350fc30ca1b1dd6f271be0a6d00e
Zachary Scott <mail@zzak.io>
2017-05-07 08:18:06 +0900

    2.0.0.rc3 release

--- rack-protection/lib/rack/protection/version.rb
+++ rack-protection/lib/rack/protection/version.rb
@@ -1,5 +1,5 @@
 module Rack
   module Protection
-    VERSION = '2.0.0.rc2'
+    VERSION = '2.0.0.rc3'
   end
 end
--- sinatra-contrib/lib/sinatra/contrib/version.rb
+++ sinatra-contrib/lib/sinatra/contrib/version.rb
@@ -1,6 +1,6 @@
 module Sinatra
   module Contrib
-    VERSION = '2.0.0.rc2'
+    VERSION = '2.0.0.rc3'
   end
 end
 
