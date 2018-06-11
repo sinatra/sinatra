e5cd58c43f44c65707e18af9aec198ef14d273e5
Jordan Owens <jkowens@gmail.com>
2018-06-11 15:59:23 -0400

    Add tests to make sure named params and captures are unescaped

--- test/routing_test.rb
+++ test/routing_test.rb
@@ -660,6 +660,18 @@ class RoutingTest < Minitest::Test
     assert ok?
   end
 
+  it 'unescapes named parameters and splats' do
+    mock_app {
+      get '/:foo/*' do |a, b|
+        assert_equal "foo\x80", params['foo']
+        assert_equal ["bar\xE2\x80\x8Cbaz"], params['splat']
+      end
+    }
+
+    get '/foo%80/bar%e2%80%8cbaz'
+    assert ok?
+  end
+
   it 'supports regular expressions' do
     mock_app {
       get(/\/foo...\/bar/) do
@@ -670,6 +682,18 @@ class RoutingTest < Minitest::Test
     get '/foooom/bar'
     assert ok?
     assert_equal 'Hello World', body
+  end
+
+  it 'unescapes regular expression captures' do
+    mock_app {
+      get(/\/foo\/(.+)/) do |path|
+        path
+      end
+    }
+
+    get '/foo/bar%e2%80%8cbaz'
+    assert ok?
+    assert_equal "bar\xE2\x80\x8Cbaz", body
   end
 
   it 'makes regular expression captures available in params[:captures]' do
