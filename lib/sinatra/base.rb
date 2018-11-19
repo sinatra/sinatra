faa09fd8799cc0f639a61ddd3055e5a4c247336f
tkmru <i.am.tkmru@gmail.com>
2018-11-19 13:37:32 +0900

    rm erubi helper and update docs

--- lib/sinatra/base.rb
+++ lib/sinatra/base.rb
@@ -682,10 +682,6 @@ module Sinatra
       render(:erb, template, options, locals, &block)
     end
 
-    def erubi(template, options = {}, locals = {}, &block)
-      render(:erubi, template, options, locals, &block)
-    end
-
     def erubis(template, options = {}, locals = {})
       warn "Sinatra::Templates#erubis is deprecated and will be removed, use #erb instead.\n" \
         "If you have Erubis installed, it will be used automatically."
