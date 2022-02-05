c922ffe2bcd5414b7af72a5e6463177892558ba2
Eloy Pérez <ej.perezgomez@gmail.com>
2022-02-05 13:10:04 +0100

    Use same rake task and config for tests in sinatra-contrib and rack-protection

--- rack-protection/spec/spec_helper.rb
+++ rack-protection/spec/spec_helper.rb
@@ -44,7 +44,7 @@ RSpec.configure do |config|
   # Print the 10 slowest examples and example groups at the
   # end of the spec run, to help surface which specs are running
   # particularly slow.
-  config.profile_examples = 10
+  config.profile_examples = 5
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
