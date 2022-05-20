3919090714d0d399bacada2f1c99537f437ba025
Tiago <cardoso_tiago@hotmail.com>
2022-05-20 12:29:46 +0100

    fixed module name

--- rack-protection/lib/rack/protection/authenticity_token.rb
+++ rack-protection/lib/rack/protection/authenticity_token.rb
@@ -20,7 +20,7 @@ module Rack
     # It is not OOTB-compatible with the {rack-csrf}[https://rubygems.org/gems/rack_csrf] gem.
     # For that, the following patch needs to be applied:
     # 
-    #   Rack::Protection::AuthenticityTokenProtection.default_options(key: "csrf.token", authenticity_param: "_csrf")
+    #   Rack::Protection::AuthenticityToken.default_options(key: "csrf.token", authenticity_param: "_csrf")
     #
     # == Options
     #
