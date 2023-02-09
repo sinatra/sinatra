8e6d11c5013e9463238c7f11463ace2a5a03eeb4
Patrik Ragnarsson <patrik@starkast.net>
2023-02-09 21:19:50 +0100

    Update routing test `handles params without a value`
    The behaviour changed in Rack 3 with
    
    - https://github.com/rack/rack/pull/1989
    - https://github.com/rack/rack/commit/ab1f1c1f912c5e434e0ac05b26140f5b8b2c0bff

--- test/routing_test.rb
+++ test/routing_test.rb
@@ -313,7 +313,7 @@ class RoutingTest < Minitest::Test
   it "handles params without a value" do
     mock_app {
       get '/' do
-        assert_nil params.fetch('foo')
+        assert_empty params.fetch('foo')
         "Given: #{params.keys.sort.join(',')}"
       end
     }
