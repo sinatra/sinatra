35957e3fac8f122c993ea2fd7922ccdc6d3f1ed3
Patrik Ragnarsson <patrik@starkast.net>
2023-08-07 09:42:21 +0200

    3.1.0 release

--- lib/sinatra/version.rb
+++ lib/sinatra/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Sinatra
-  VERSION = '3.0.6'
+  VERSION = '3.1.0'
 end
--- rack-protection/lib/rack/protection/version.rb
+++ rack-protection/lib/rack/protection/version.rb
@@ -2,6 +2,6 @@
 
 module Rack
   module Protection
-    VERSION = '3.0.6'
+    VERSION = '3.1.0'
   end
 end
