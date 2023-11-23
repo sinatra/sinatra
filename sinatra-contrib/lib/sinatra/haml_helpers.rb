cea2d6223eec2c6065d961e1b69c52f7a592e0f6
Patrik Ragnarsson <patrik@starkast.net>
2023-11-23 07:52:35 +0100

    Fix typo in docs
    [ci skip]
    
    Co-authored-by: Olle Jonsson <olle.jonsson@gmail.com>

--- sinatra-contrib/lib/sinatra/haml_helpers.rb
+++ sinatra-contrib/lib/sinatra/haml_helpers.rb
@@ -7,7 +7,7 @@ module Sinatra
   # = Sinatra::HamlHelpers
   #
   # This extension provides some of the helper methods that existed in Haml 5
-  # but was removed in Haml 6. To use this in your app, just +register+ it:
+  # but were removed in Haml 6. To use this in your app, just +register+ it:
   #
   #   require 'sinatra/base'
   #   require 'sinatra/haml_helpers'
