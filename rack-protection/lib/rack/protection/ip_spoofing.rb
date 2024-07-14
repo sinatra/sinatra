079f6929af45dfe4bf3ed8f8c61cb8be527b0299
Malik Parvez <84777619+malikparvez@users.noreply.github.com>
2024-07-15 01:29:23 +0530

    Adding support for HTTP_FORWARDED

--- rack-protection/lib/rack/protection/ip_spoofing.rb
+++ rack-protection/lib/rack/protection/ip_spoofing.rb
@@ -14,13 +14,38 @@ module Rack
       default_reaction :deny
 
       def accepts?(env)
-        return true unless env.include? 'HTTP_X_FORWARDED_FOR'
+        return true unless env.include?('HTTP_X_FORWARDED_FOR') || env.include?('HTTP_FORWARDED')
 
-        ips = env['HTTP_X_FORWARDED_FOR'].split(',').map(&:strip)
+        ips = if env['HTTP_FORWARDED']
+          parse_forwarded(env['HTTP_FORWARDED'])
+        else
+          env['HTTP_X_FORWARDED_FOR'].split(',').map(&:strip)
+        end
+
         return false if env.include?('HTTP_CLIENT_IP') && (!ips.include? env['HTTP_CLIENT_IP'])
         return false if env.include?('HTTP_X_REAL_IP') && (!ips.include? env['HTTP_X_REAL_IP'])
 
         true
+      end
+
+      private
+
+      def parse_forwarded(forwarded_header)
+        return nil unless forwarded_header
+
+        ips = []
+
+        forwarded_header.to_s.split(/\s*;\s*/).each do |field|
+          field.split(/\s*,\s*/).each do |key|
+            # Use a regex to match and capture 'for' and 'by' fields along with their IP addresses
+            if match = key.match(/\A\s*(by|for)\s*=\s*"?\[?([^\]]+)\]?"?\s*\Z/i)
+              # Extract the IP address and strip any surrounding whitespace
+              ips << match[2].strip
+            end
+          end
+        end
+
+        ips
       end
     end
   end
