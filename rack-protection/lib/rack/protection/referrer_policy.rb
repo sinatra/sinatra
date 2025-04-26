7cdef7ae8f170ee484185ab7983742d5d52446c3
Dave Copeland <davec@naildrivin5.com>
2025-04-26 19:16:13 -0400

    slight improvement to referrer policy docs

--- rack-protection/lib/rack/protection/referrer_policy.rb
+++ rack-protection/lib/rack/protection/referrer_policy.rb
@@ -6,14 +6,20 @@ module Rack
   module Protection
     ##
     # Prevented attack::   Secret leakage, third party tracking
-    # Supported browsers:: mixed support
-    # More infos::         https://www.w3.org/TR/referrer-policy/
-    #                      https://caniuse.com/#search=referrer-policy
+    # Supported browsers:: Most modern browsers from 2018 onwards.  See https://caniuse.com/#search=referrer-policy for
+    #                      specifics.
+    # More info::          * https://www.w3.org/TR/referrer-policy/
+    #                      * https://developer.mozilla.org/en-US/docs/Web/Security/Referer_header:_privacy_and_security_concerns
     #
-    # Sets Referrer-Policy header to tell the browser to limit the Referer header.
+    # Sets the {Referrer-Policy}[https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Referrer-Policy]
+    # header to tell the browser to limit the Referer header.
     #
-    # Options:
-    # referrer_policy:: The policy to use (default: 'strict-origin-when-cross-origin')
+    # == Options
+    # 
+    # [<tt>:referrer_policy</tt>] The policy to use as a String. Default is 'strict-origin-when-cross-origin' (which,
+    #                             despite being the browser's default, is still set explicitly when 
+    #                             this middleware is used). Note that this option has no effect if an upstream
+    #                             middleware (or your app) has set the Referrer-Policy header.
     class ReferrerPolicy < Base
       default_options referrer_policy: 'strict-origin-when-cross-origin'
 
